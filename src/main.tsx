ef697eea33477e880a6500b9f5d6ad9ead8635e8
josesouza96 <jose.a.s.amador@gmail.com>
2026-04-03 23:40:11 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,5 @@
-import { createHead, UnheadProvider } from '@unhead/react/client'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
-const head = createHead()
-
-createRoot(document.getElementById('root')!).render(
-  <UnheadProvider head={head}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </UnheadProvider>
-)
+createRoot(document.getElementById('root')!).render(<App />)
